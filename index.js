708c015e99bb7cca524f1e638a59a149dda81b55
RafaelTDK <rafaelhs160703@gmail.com>
2024-05-02 21:21:37 -0600

    Comit Finaaaaal

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-// Hola Prueba 2
 import express from 'express';
 import { engine } from 'express-handlebars';
 import path from 'path';
-import {fileURLToPath} from 'url';
+import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
 dotenv.config()
 
 const app = express();
 const port = process.env.PORT || 3000;
-const __filename =  fileURLToPath(import.meta.url);
-const _dirname = path.dirname(_filename);
+
+// Obtener la ruta del archivo actual y su directorio
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const images = [
     { src: "/img/img1.jpeg", url: "https://www.youtube.com/watch?v=aZLI14nFC4E" },
     { src: "/img/img2.jpeg", url: "https://www.youtube.com/watch?v=ga5Bo4YdgH4" },
@@ -21,17 +23,18 @@ app.engine('handlebars', engine({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
-app.use(express.static(__dirname + "/public"));
+// Usar el directorio actual para servir archivos estáticos
+app.use(express.static(path.join(__dirname, "public")));
 
 // Ruta principal 
 app.get('/raiz', (req, res) => {
-    res.render("raiz");
+    res.render("titulo");
 });
 
 // Ruta adicional de las imagenes
-app.get("/ruta-alterna/", (req, res) => {
-    const randomImage = images[Math.floor(Math.random()*images.length)]
-    res.render("ruta-alterna", {image: randomImage});
+app.get("/rutaalterna", (req, res) => {
+    const randomImage = images[Math.floor(Math.random() * images.length)]
+    res.render("saludo", { image: randomImage });
 });
 
 // Custom 404 page 
